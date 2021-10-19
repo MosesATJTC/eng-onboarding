47926b564260b541d5932d9858ba55402615abe2
Moses <92710832+MosesATJTC@users.noreply.github.com>
2021-10-19 19:18:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-const prompt = require("prompt-sync")()
-function fib(n){
-    let ele1 = 0
-    let ele2= 1
-    let temp = 0
-    for(let i =0;i<n;i++){
-        console.log(ele1)
-        temp  = ele1+ele2
-        ele1 = ele2
-        ele2 = temp
-    }
+const prompt = require("prompt-sync")();
+const fib = (n) => {
+  if (n == 0) {
+    console.log("number should be greater than zero");
+    return;
+  }
+  let firstElement = 0;
+  let secondElement = 1;
+  let temp = 0;
+  for (let i = 0; i < n; i++) {
+    console.log(firstElement);
+    temp = firstElement + secondElement;
+    firstElement = secondElement;
+    secondElement = temp;
+  }
+};
+try {
+  const num = parseInt(prompt("enter the number :"));
+  if (num == NaN) console.log("enter valid number");
+  fib(num);
+} catch (error) {
+  console.log(error);
 }
-const num = parseInt(prompt("enter the number :"))
-fib(num)
