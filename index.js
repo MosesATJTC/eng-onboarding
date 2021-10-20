78cc66417f8c250d3f0007b8629ab92e519d316e
Moses <92710832+MosesATJTC@users.noreply.github.com>
2021-10-20 10:56:22 +0530

    printing fibonacci_sequence (#1)
    * printing fibonacci_sequence
    
    * Update pull_request_template.md
    
    * Update index.js
    
    Co-authored-by: Codetoinnovate <codetoinnovate@Codetoinnovates-MacBook-Air.local>

---
+++ index.js
@@ -0,0 +1,23 @@
+const prompt = require("prompt-sync")();
+const fib = (n) => {
+  if (n == 0) {
+    console.log("number should be greater than zero");
+    return;
+  }
+  let firstElement = 0;
+  let secondElement = 1;
+  let temp = 0;
+  for (let i = 0; i < n; i++) {
+    console.log(firstElement);
+    temp = firstElement + secondElement;
+    firstElement = secondElement;
+    secondElement = temp;
+  }
+};
+try {
+  const num = parseInt(prompt("enter the number :"));
+  if (num == NaN) console.log("enter valid number");
+  fib(num);
+} catch (error) {
+  console.log(error);
+}
